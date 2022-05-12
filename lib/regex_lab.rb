c5cd1491b092bdc470d0a81252e8fd1ccf892870
Zach <shaw.zacht@gmail.com>
2022-05-12 15:05:39 -0700

    commit tree fcc3a999e748375e92501055c3691bd000b1115e

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,43 @@
 def starts_with_a_vowel?(word)
-
+  first_letter = word[0]
+  if first_letter.downcase.match(/[^aeiou]/)
+    false
+  else 
+    true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    lower = text.downcase
+    lower.scan(/\w+ing/) 
+    #lower.slice(0, 2).match(/un/) && lower.slice(-3, lower.length).match(/ing/)
 end
 
 def words_five_letters_long(text)
-
+    split_string = text.split
+    split_string.grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/^[A-Z]/) && text.match(/\W\z/)
+        true
+    else
+        false
+    end
 end
 
 def valid_phone_number?(phone)
+    nums = phone.gsub(/[^0-9]/, '')
 
+    if nums.match(/(\d{10})/)
+        true
+    else
+        false
+    end
+
+    # if phone.match(/^\d{10}$/)
+    #     true
+    # else 
+    #     false
+    # end
 end
